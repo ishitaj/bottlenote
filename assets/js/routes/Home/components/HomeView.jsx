ee983628ca08935d781dc515743c4392e07cd447
ijain89 <ijain89@gmail.com>
2017-10-21 03:57:18 +0530

    Modifying HomePage to include image

--- assets/js/routes/Home/components/HomeView.jsx
+++ assets/js/routes/Home/components/HomeView.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import bottleNoteLogo from '../../../public/bottleNote.png'
+import NotePage from '../../../public/NotePage.png'
 import Style from '../../../css/style.css'
 
 export default () => (
@@ -9,7 +10,9 @@ export default () => (
       <h1 className="App-title">Welcome to bottleNote!</h1>
     </header>
     <p className="App-intro">
-      Your personal <a href="http://news.nationalgeographic.com/news/2013/08/130806-dolphins-memories-animals-science-longest/">bottlenose dolphin</a> helps you remember new words in multiple languages!
+      Your personal <a href="http://news.nationalgeographic.com/news/2013/08/130806-dolphins-memories-animals-science-longest/">bottlenose dolphin</a> helps you remember new words in multiple languages!<br/>
+      BOTTLENOSE DOLPHINS have the longest memory in our Animal Kingdom! Don't ever forget a word with the bottleNote Note-Taking app for language-learners. You can capture words in multiple languages with their definitions and context.<br/>
+      <img src={NotePage} className="img-responsive" alt="bottleNote" />
     </p>
   </div>
 )
